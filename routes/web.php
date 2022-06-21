b2999d3eccb53e4ed89a430902a3f7a2c30a93b7
christianaguilargithub <aguilar.chris9123@gmail.com>
2022-06-21 20:58:55 +0800

    dfsgfsdg

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ Route::get('/', function () {
 });
 
 Route::resource('products', ProductController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
